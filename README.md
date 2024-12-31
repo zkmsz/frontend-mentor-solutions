428e8895d78e2ec67f64728e54b3c8c28b8e4098
zkmsz <kristopher.shannon@gmail.com>
2024-12-31 06:16:49 -0700

    finished article preview component

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
 - [Product Preview Card Component](./product-preview-card-component-main/)
 - [Four Card Feature Section](./four-card-feature-section-master/)
 - [Meet Landing Page ](./meet-landing-page-next/)
+- [Article Preview Component](./article-preview-component-master/)
 
 ### Junior
 
--- article-preview-component-master/js/script.js
+++ article-preview-component-master/js/script.js
@@ -2,11 +2,11 @@ const share = document.getElementById("share-icon");
 
 const authorFooter = document.getElementById("author-footer");
 const shareFooter = document.getElementById("share-footer");
-const shareIcon = document.getElementById("share-icon");
+const desktopTooltip = document.getElementById("desktop-tooltip");
 
 const toggleShare = () => {
   if (window.innerWidth >= 1024) {
-    console.log(window.innerWidth);
+    desktopTooltip.classList.toggle("hidden");
   } else {
     authorFooter.classList.toggle("hidden");
     shareFooter.classList.toggle("hidden");
